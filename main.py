66247ba7b01c944dcecf82ccab50ba2a45c4d7a7
Timmy Ryan <97642935+Timmy0x@users.noreply.github.com>
2024-03-22 17:13:38 -0600

    removed events handler

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-import threading
-import sys
-import os
-import json
 
 from node import Node
-from events import EventsHandler
 
-from errorcallbacks import restart
-
-events_handler = EventsHandler()
-events_handler_thread = threading.Thread(target=events_handler.start)
-events_handler_thread.start()
-
-node = Node(models=[12, 14, 16])
+node = Node(models=[12, 14, 16], threads=2048)
 node.start()
